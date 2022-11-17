195885469833dab19b695a47d07de2bdceea8b1d
127-0-0-vvk <vvkz.1818@gmail.com>
2022-11-17 19:04:44 +0000

    fixing bad indentation

--- devopslib/randomfruit.py
+++ devopslib/randomfruit.py
@@ -5,6 +5,7 @@ def fruit():
     fruits = ["apple", "cherry", "melon"]
     return choices(fruits)[0]
 
+
 def meal(beverage):
     my_fruit = fruit()
     print(f" Your fruit is {my_fruit} and your beverage is {beverage}")
