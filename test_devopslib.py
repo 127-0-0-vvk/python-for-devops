0c76172cb8dd898eec739db8ffd42a1d569fcd04
Vivek <41470324+127-0-0-vvk@users.noreply.github.com>
2022-11-17 06:07:00 +0000

    test case pytest added

--- test_devopslib.py
+++ test_devopslib.py
@@ -2,5 +2,5 @@ from devopslib.randomfruit import fruit
 
 def test_fruit():
     fruit_choice = fruit()
-    assert fruit_choice inclear ["apple", "cherry", "melon"]
+    assert fruit_choice in ["apple", "cherry", "melon"]
     
